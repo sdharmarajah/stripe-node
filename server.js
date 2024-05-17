cba0678970437b64abd9025184398c3de7604b0c
sohan dharmarajah <sdharmarajah@gmail.com>
2024-05-18 05:26:53 +0530

    Added back the /create-payment-intent route

--- server.js
+++ server.js
@@ -36,32 +36,32 @@ app.post('/create-checkout-session', async (req, res) => {
   res.redirect(303, session.url);
 });
 
-app.listen(4242, () => console.log('Running on port 4242'));
+// app.listen(4242, () => console.log('Running on port 4242'));
 
 
-// const calculateOrderAmount = (items) => {
-//   // Replace this constant with a calculation of the order's amount
-//   // Calculate the order total on the server to prevent
-//   // people from directly manipulating the amount on the client
-//   return 1400;
-// };
+const calculateOrderAmount = (items) => {
+  // Replace this constant with a calculation of the order's amount
+  // Calculate the order total on the server to prevent
+  // people from directly manipulating the amount on the client
+  return 1400;
+};
 
-// app.post("/create-payment-intent", async (req, res) => {
-//     const { items } = req.body;
+app.post("/create-payment-intent", async (req, res) => {
+    const { items } = req.body;
 
-//     // Create a PaymentIntent with the order amount and currency
-//     const paymentIntent = await stripe.paymentIntents.create({
-//         amount: calculateOrderAmount(items),
-//         currency: "usd",
-//         // In the latest version of the API, specifying the `automatic_payment_methods` parameter is optional because Stripe enables its functionality by default.
-//         automatic_payment_methods: {
-//             enabled: true,
-//         },
-//     });
+    // Create a PaymentIntent with the order amount and currency
+    const paymentIntent = await stripe.paymentIntents.create({
+        amount: calculateOrderAmount(items),
+        currency: "usd",
+        // In the latest version of the API, specifying the `automatic_payment_methods` parameter is optional because Stripe enables its functionality by default.
+        automatic_payment_methods: {
+            enabled: true,
+        },
+    });
 
-//     res.send({
-//         clientSecret: paymentIntent.client_secret,
-//     });
-// });
+    res.send({
+        clientSecret: paymentIntent.client_secret,
+    });
+});
 
-// app.listen(4242, () => console.log("Node server listening on port 4242!"));
+app.listen(4242, () => console.log("Node server listening on port 4242!"));
