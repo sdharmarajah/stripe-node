60454136c74116f4214ca24ed42eebd046e7e93e
ahmednabil310 <58309891+ahmednabil310@users.noreply.github.com>
2024-05-17 09:41:50 +0300

    Enable CORS

--- server.js
+++ server.js
@@ -1,37 +1,45 @@
+const express = require("express");
+const app = express();
+var cors = require("cors");
+
 // This is a public sample test API key.
 // Don’t submit any personally identifiable information in requests made with this key.
 // Sign in to see your own test API key embedded in code samples.
-const stripe = require('stripe')('sk_test_4eC39HqLyjWDarjtT1zdp7dc');
-const express = require('express');
-const app = express();
-app.use(express.static('public'));
+const stripe = require("stripe")(
+    "sk_test_51LZThuJyDRgsgtRA3iz5HcMLGPRfJvMFBj1oSZsXY1RxAYPywCPsGzJKq99rHFu2uM3Oj8r7bQRisJmYKhzTqVZi00t7lWupTH"
+);
 
-const YOUR_DOMAIN = 'http://localhost:4242';
+app.use(express.static("public"));
+app.use(express.json());
+app.use(
+    cors({
+        origin: ["http://localhost:3000"],
+    })
+);
 
-app.post('/create-checkout-session', async (req, res) => {
-  const session = await stripe.checkout.sessions.create({
-    ui_mode: 'embedded',
-    line_items: [
-      {
-        // Provide the exact Price ID (for example, pr_1234) of the product you want to sell
-        price: '{{PRICE_ID}}',
-        quantity: 1,
-      },
-    ],
-    mode: 'payment',
-    return_url: `${YOUR_DOMAIN}/return.html?session_id={CHECKOUT_SESSION_ID}`,
-  });
+const calculateOrderAmount = (items) => {
+    // Replace this constant with a calculation of the order's amount
+    // Calculate the order total on the server to prevent
+    // people from directly manipulating the amount on the client
+    return 1400;
+};
 
-  res.send({clientSecret: session.client_secret});
+app.post("/create-payment-intent", async (req, res) => {
+    const { items } = req.body;
+
+    // Create a PaymentIntent with the order amount and currency
+    const paymentIntent = await stripe.paymentIntents.create({
+        amount: calculateOrderAmount(items),
+        currency: "usd",
+        // In the latest version of the API, specifying the `automatic_payment_methods` parameter is optional because Stripe enables its functionality by default.
+        automatic_payment_methods: {
+            enabled: true,
+        },
+    });
+
+    res.send({
+        clientSecret: paymentIntent.client_secret,
+    });
 });
 
-app.get('/session-status', async (req, res) => {
-  const session = await stripe.checkout.sessions.retrieve(req.query.session_id);
-
-  res.send({
-    status: session.status,
-    customer_email: session.customer_details.email
-  });
-});
-
-app.listen(4242, () => console.log('Running on port 4242'));
+app.listen(4242, () => console.log("Node server listening on port 4242!"));
