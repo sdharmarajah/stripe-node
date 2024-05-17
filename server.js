b166bed790949700bcc240069e0da4c7fbfcfbcf
sohan dharmarajah <sdharmarajah@gmail.com>
2024-05-17 20:07:18 +0530

    Stripe-hosted checkout

--- server.js
+++ server.js
@@ -17,29 +17,51 @@ app.use(
     })
 );
 
-const calculateOrderAmount = (items) => {
-    // Replace this constant with a calculation of the order's amount
-    // Calculate the order total on the server to prevent
-    // people from directly manipulating the amount on the client
-    return 1400;
-};
+const YOUR_DOMAIN = 'http://localhost:4242';
 
-app.post("/create-payment-intent", async (req, res) => {
-    const { items } = req.body;
+app.post('/create-checkout-session', async (req, res) => {
+  const session = await stripe.checkout.sessions.create({
+    line_items: [
+      {
+        // Provide the exact Price ID (for example, pr_1234) of the product you want to sell
+        price: '{{PRICE_ID}}',
+        quantity: 1,
+      },
+    ],
+    mode: 'payment',
+    success_url: `${YOUR_DOMAIN}?success=true`,
+    cancel_url: `${YOUR_DOMAIN}?canceled=true`,
+  });
 
-    // Create a PaymentIntent with the order amount and currency
-    const paymentIntent = await stripe.paymentIntents.create({
-        amount: calculateOrderAmount(items),
-        currency: "usd",
-        // In the latest version of the API, specifying the `automatic_payment_methods` parameter is optional because Stripe enables its functionality by default.
-        automatic_payment_methods: {
-            enabled: true,
-        },
-    });
-
-    res.send({
-        clientSecret: paymentIntent.client_secret,
-    });
+  res.redirect(303, session.url);
 });
 
-app.listen(4242, () => console.log("Node server listening on port 4242!"));
+app.listen(4242, () => console.log('Running on port 4242'));
+
+
+// const calculateOrderAmount = (items) => {
+//   // Replace this constant with a calculation of the order's amount
+//   // Calculate the order total on the server to prevent
+//   // people from directly manipulating the amount on the client
+//   return 1400;
+// };
+
+// app.post("/create-payment-intent", async (req, res) => {
+//     const { items } = req.body;
+
+//     // Create a PaymentIntent with the order amount and currency
+//     const paymentIntent = await stripe.paymentIntents.create({
+//         amount: calculateOrderAmount(items),
+//         currency: "usd",
+//         // In the latest version of the API, specifying the `automatic_payment_methods` parameter is optional because Stripe enables its functionality by default.
+//         automatic_payment_methods: {
+//             enabled: true,
+//         },
+//     });
+
+//     res.send({
+//         clientSecret: paymentIntent.client_secret,
+//     });
+// });
+
+// app.listen(4242, () => console.log("Node server listening on port 4242!"));
